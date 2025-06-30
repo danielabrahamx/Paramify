3122a66278b48f7325b71e5303210a2dec703b3f
danielabrahamx <danielftabraham@outlook.com>
2025-06-30 12:20:00 +0100

    Migrate to Arbitrum Sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,13 +3,26 @@ require("dotenv").config();
 
 module.exports = {
   solidity: {
-    version: "0.8.24",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
-    },
+      {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
   paths: {
     sources: "./contracts",
@@ -21,6 +34,11 @@ module.exports = {
     hardhat: {},
     localhost: {
       url: "http://127.0.0.1:8545",
-    }
+    },
+    arbitrumSepolia: {
+      url: process.env.ARBITRUM_SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY && process.env.PRIVATE_KEY.length === 66 ? [process.env.PRIVATE_KEY] : [],
+      chainId: 421614,
+    },
   },
 };
